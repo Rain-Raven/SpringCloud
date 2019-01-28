07974485ec10375a035d34545346373fd09b59c9
Administrator <11>
2019-01-28 16:09:03 +0800

    嗯嗯嗯

--- consumers/src/main/java/com/zxa/consumers/controller/HelloWorld.java
+++ consumers/src/main/java/com/zxa/consumers/controller/HelloWorld.java
@@ -1,14 +1,17 @@
 package com.zxa.consumers.controller;
 
 import com.zxa.entitis.Person;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class HelloWorld {
 
-
+    @Autowired
+    RestTemplate restTemplate;
     @RequestMapping(value = "/la",method = {RequestMethod.GET,RequestMethod.POST})
     public Person helloWorld(){
         Person person=new Person();
