0900e3586d785bdae4e2c076629a2e0809ddc2e0
Administrator <11>
2019-02-14 15:35:18 +0800

    添加了熔断机制

--- api/src/main/java/com/zxa/fallback/EmailServiceFallback.java
+++ api/src/main/java/com/zxa/fallback/EmailServiceFallback.java
@@ -2,12 +2,11 @@ package com.zxa.fallback;
 
 import com.zxa.service.EmailService;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Component
 public class EmailServiceFallback implements EmailService {
     @Override
-    public boolean bindingEmail(@RequestParam(value = "toEmail") String toEmail,@RequestParam(value = "captcha") String captcha) {
+    public boolean bindingEmail(String toEmail,String captcha) {
         return false;
     }
 }
